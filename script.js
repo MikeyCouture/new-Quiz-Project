2c175743eab86d348b71116bc978a00413e96366
MikeyCouture <michelcouture42@gmail.com>
2021-03-28 18:55:33 -0400

    Start on functionality

---
+++ script.js
@@ -0,0 +1,94 @@
+"use strict";
+const quizContainer = document.getElementById("question-container");
+const mainQuestion = document.getElementById("main-Question");
+const mainLabel = document.getElementById("main-Label");
+const button = document.getElementById("button");
+const buttTwo = document.getElementById("buttonTwo");
+const answerMain = document.querySelector(".answers");
+const form = document.querySelector(".form-container");
+const scoreCard = document.querySelector(".scoreCard");
+
+const question1 = {
+  question: "How many seasons of Friends are there?",
+  answers: [10, 11, 8],
+  correctAnswer: 10
+};
+
+const question2 = {
+  question:
+    "What was the name of the first restaurant Monica was head chef at?",
+  answers: ["Iridium", "Alessandro’s", "Manhattan Restaurant"],
+  correctAnswer: "Alessandro’s"
+};
+
+const question3 = {
+  question: "What animal does Chanler not like?",
+  answers: ["Cats", "Dogs", "Birds"],
+  correctAnswer: "Dogs"
+};
+
+const question4 = {
+  question: "How many sisters does Joey have?",
+  answers: [4, 5, 7],
+  correctAnswer: 7
+};
+
+const friendsQuestions = [question1, question2, question3, question4];
+
+let score = 0;
+let currentQuestion;
+
+//---------------------------------------------------------------------------
+button.addEventListener("click", function(e) {
+  // button.textContent = "Next Question!";
+  // quizContainer.innerHTML = "";
+  let randNum = Math.floor(Math.random() * friendsQuestions.length);
+  currentQuestion = friendsQuestions[randNum];
+
+  const html = `<p id="main-Question" class="question">${currentQuestion.question}</p >`;
+  quizContainer.insertAdjacentHTML("beforeend", html);
+
+  showAnswer(currentQuestion.answers);
+  button.classList.add("noShow");
+});
+
+//---------------------------------------------------------------------------
+
+const showAnswer = function(answerList) {
+  const answerOptions = answerList.map((ans, i) => {
+    return `<input class="inputClass" type="radio" name="question" id="${i}" value="${ans}" /> <label for="" id="main-Label">${ans}</label>`;
+  });
+
+  quizContainer.insertAdjacentHTML("beforeend", answerOptions);
+};
+
+//---------------------------------------------------------------------------
+
+buttTwo.addEventListener("click", function(e) {
+  const Ans = document.querySelector("input[name='question']:checked").value;
+  const index = friendsQuestions.indexOf(currentQuestion);
+  if (index > -1) {
+    friendsQuestions.splice(index, 1);
+    console.log(friendsQuestions);
+  }
+
+  if (Ans == currentQuestion.correctAnswer) {
+    console.log("Correct!");
+    score = score + 1;
+    scoreCard.textContent = `Score: ${score}`;
+    console.log(score);
+  } else {
+    console.log(`sorry, the answer is ${currentQuestion.correctAnswer}`);
+    console.log(score);
+  }
+
+  quizContainer.innerHTML = "";
+  let randNum = Math.floor(Math.random() * friendsQuestions.length);
+
+  currentQuestion = friendsQuestions[randNum];
+
+  const html = `<p id="main-Question" class="question">${currentQuestion.question}</p >`;
+  quizContainer.insertAdjacentHTML("beforeend", html);
+
+  showAnswer(currentQuestion.answers);
+});
